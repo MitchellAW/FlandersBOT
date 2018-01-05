1300ce63ee5f76f485f5a21f02c2508e622ed5cf
Mitch Woollatt <21175347+MitchellAW@users.noreply.github.com>
2018-01-06 10:09:56 +1030

    Updated readme and help command with the gif commands

--- README.md
+++ README.md
@@ -16,6 +16,10 @@ Will post a random Simpsons quote with accompanying picture.
 
 Searches for a Simpsons quote using all text following the command, grabs the first result and sends the resulting quote with accompanying picture.
 
+### !simpsonsgif
+
+Will post a random Simpsons quote with accompanying gif.
+
 ### !futurama
 
 Will post a random Futurama quote with accompanying picture.
@@ -24,6 +28,10 @@ Will post a random Futurama quote with accompanying picture.
 
 Searches for a Futurama quote using all text following the command, grabs the first result and sends the resulting quote with accompanying picture.
 
+### !futuramagif
+
+Will post a random Futurama quote with accompanying gif.
+
 ### !rickandmorty
 
 Will post a random Rick and Morty quote with accompanying picture.
@@ -31,3 +39,7 @@ Will post a random Rick and Morty quote with accompanying picture.
 ### !rickandmorty [quote to search for]
 
 Searches for a Rick and Morty quote using all text following the command, grabs the first result and sends the resulting quote with accompanying picture.
+
+### !rickandmortygif
+
+Will post a random Rick and Morty quote with accompanying gif.
--- commands.py
+++ commands.py
@@ -13,14 +13,24 @@ Will post a random Simpsons quote with accompanying picture.
 `!simpsons [quote to search for]`
 Searches for a Simpsons quote using all text following the command, grabs the first result and sends the resulting quote with accompanying picture.
 
+`!simpsonsgif`
+Will post a random Simpsons quote with accompanying gif.
+
 `!futurama`
 Will post a random Futurama quote with accompanying picture.
 
 `!futurama [quote to search for]`
 Searches for a Futurama quote using all text following the command, grabs the first result and sends the resulting quote with accompanying picture.
 
+`!futuramagif`
+Will post a random Futurama quote with accompanying gif.
+
 `!rickandmorty`
 Will post a random Rick and Morty quote with accompanying picture.
 
 `!rickandmorty [quote to search for]`
-Searches for a Rick and Morty quote using all text following the command, grabs the first result and sends the resulting quote with accompanying picture.'''
+Searches for a Rick and Morty quote using all text following the command, grabs the first result and sends the resulting quote with accompanying picture.
+
+`!rickandmortygif`
+Will post a random Rick and Morty quote with accompanying gif.
+'''
