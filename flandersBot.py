371c0d0f95f13590df4d64805bc1d6f7ea59a9f5
Mitch Woollatt <21175347+MitchellAW@users.noreply.github.com>
2018-01-19 11:46:43 +1030

    Rewritten to use commands from discord.ext

--- flandersBot.py
+++ flandersBot.py
@@ -1,10 +1,105 @@
 import asyncio
 import config
 import discord
-import info
+import infoimport asyncio
+import config
+import discord
+import botInfo
 
 from cartoons import CartoonAPI
+from discord.ext import commands
 
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'))
+bot.remove_command('help')
+
+# Create APIs for simpsons, futurama and rick & morty
+frinkiac = CartoonAPI('simpsons', 'https://frinkiac.com/')
+morbotron = CartoonAPI('futurama', 'https://morbotron.com/')
+masterOfAllScience = CartoonAPI('rickandmorty',
+                                'https://masterofallscience.com/')
+
+@bot.event
+async def on_ready():
+    print('FlandersBOT Logged in.')
+    print('Username: ' + str(bot.user.name))
+    print('Client ID: ' + str(bot.user.id))
+    print(('Invite URL: '
+           + 'https://discordapp.com/oauth2/authorize?&client_id='
+           + bot.user.id + '&scope=bot&permissions=19456'))
+
+# Whispers a description of the bot with author, framework, server count etc.
+@bot.command()
+async def info():
+    await bot.whisper(botInfo.botInfo + '\nServers: ' + str(len(bot.servers)))
+
+# Whispers a list of the bot commands
+@bot.command()
+async def help():
+    await bot.whisper(botInfo.commandsList)
+
+# Searches for a simpsons quote using message following prefix, messages a gif
+# of first search result. Messages a random simspons image if no search
+# arguments given.
+@bot.command()
+async def simpsons(*, message : str=None):
+    if message == None:
+        await bot.say(await frinkiac.getRandomCartoon())
+
+    else:
+        await bot.say(await frinkiac.findCartoonQuote(message, True))
+
+# Messages a random simpsons quote with accomanying gif
+@bot.command()
+async def simpsonsgif():
+    await bot.say(await frinkiac.getRandomCartoon(True))
+
+# Searches for a futurama quote using message following prefix, messages a gif
+# of first search result. Messages a random futurama image if no search
+# arguments given.
+@bot.command()
+async def futurama(*, message : str=None):
+    if message == None:
+        await bot.say(await morbotron.getRandomCartoon())
+
+    else:
+        await bot.say(await morbotron.findCartoonQuote(message, True))
+
+# Messages a random futurama quote with accomanying gif
+@bot.command()
+async def futuramagif():
+    await bot.say(await morbotron.getRandomCartoon(True))
+
+# Searches for a rick and morty quote using message following prefix, messages
+# a gif of first search result. Messages a random rick and morty image if no
+# search arguments given.
+@bot.command()
+async def rickandmorty(*, message : str=None):
+    if message == None:
+        await bot.say(await masterOfAllScience.getRandomCartoon())
+
+    else:
+        await bot.say(await masterOfAllScience.findCartoonQuote(message, True))
+
+# Messages a random rick and morty quote with accomanying gif
+@bot.command()
+async def rickandmortygif():
+    await bot.say(await masterOfAllScience.getRandomCartoon(True))
+
+# Shuts the bot down - only usable by the bot owner specified in config
+@bot.command(pass_context=True)
+async def shutdown(ctx):
+    if ctx.message.author.id == config.OWNERID:
+        await bot.whisper("Shutting down. Bye!")
+        await bot.logout()
+        await bot.close()
+
+bot.run(config.TOKEN)
+
+
+from cartoons import CartoonAPI
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'))
 client = discord.Client()
 
 # Create APIs for simpsons, futurama and rick & morty
@@ -12,7 +107,7 @@ frinkiac = CartoonAPI('simpsons', 'https://frinkiac.com/')
 morbotron = CartoonAPI('futurama', 'https://morbotron.com/')
 masterOfAllScience = CartoonAPI('rickandmorty', 'https://masterofallscience.com/')
 
-@client.event
+@bot.event
 async def on_ready():
     print('FlandersBOT Logged in.')
     print('Username: ' + str(client.user.name))
@@ -20,19 +115,15 @@ async def on_ready():
     print(('Invite URL: ' + 'https://discordapp.com/oauth2/authorize?&client_id='
            + client.user.id + '&scope=bot&permissions=19456'))
 
-@client.event
-async def on_message(message):
-    prefixFound = False
-    # Allow use of an @mention to the bot as a prefix (Useful if any commands
-    # clash with other bots)
-    if message.content.startswith('<@' + str(config.BOTID) + '>'):
-        prefixFound = True
-        message.content = message.content[22:].lower()
 
-    # Remove prefix and change to lowercase so commands are case-insensitive
-    if message.content.startswith(config.COMMANDPREFIX):
-        prefixFound = True
-        message.content = message.content[1:].lower()
+@bot.command(pass_context=True)
+async def shutdown(ctx):
+    if ctx.message.author.id == config.OWNERID:
+        await bot.say("Shutting down. Bye!")
+        await bot.logout()
+        await bot.close()
+
+
 
     # If corrent prefix and the message author isn't a bot, check if command
     # was executed
