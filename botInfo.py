61ee744fc5d18ca4e2a00e4d41686e4a0ba56d00
Mitch Woollatt <21175347+MitchellAW@users.noreply.github.com>
2018-01-20 21:53:37 +1030

    Revised help and info text to be more compact

--- botInfo.py
+++ botInfo.py
@@ -1,62 +1,41 @@
 import discord
 
 commandsList = '''
-Hi-diddly-ho, neighborino! Here are the commands, shout them out anytime and I'll
-happily oblige! Wel-diddly-ell, so long as the reverend approves of course.
+Hi-diddly-ho, neighborino! Here are the commands, shout them out anytime and I'll happily oblige! Well, so long as the reverend approves of course.
 
-Usage: ned [command], <@221609683562135553> [command]
+__**COMMAND PREFIXES**__
+
+ned, diddly, doodly
+
+*For a full description, visit:*
+<https://github.com/MitchellAW/FlandersBOT/blob/master/README.md#usage>
 
 __**COMMANDS**__
 
-*All commands must start with one of the command prefixes or <@221609683562135553>*
+***All commands must start with one of the command prefixes or <@221609683562135553>!***
 
-`help`
-Will send a personal mesage to the sender with a list of the commands.
+**help** - Will send this list of commands.
+**info** - Will send a personal message with more information about the bot.
+**prefix** - Will post the prefixes this bot uses.
 
-`info`
-Will send a personal message to the sender with more information regarding the bot.
-(Framework, Author, invite URL etc.)
+**simpsons** - Will post a random Simpsons moment with caption.
+**simpsons [quote]** - Searches for a Simpsons gif using the quote.
+**simpsonsgif** - Will post a random Simpsons gif with caption.
 
-`prefix`
-Will post the prefixes this bot uses.
+**futurama** - Will post a random Futurama moment with caption.
+**futurama [quote]** - Searches for a Futurama gif using the quote.
+**futuramagif** - Will post a random Futurama gif with caption.
 
-`simpsons`
-Will post a random Simpsons quote with accompanying picture.
+**rickandmorty** - Will post a random Rick and Morty moment with caption.
+**rickandmorty [quote]** - Searches for a Rick and Morty gif using the quote.
+**rickandmortygif** - Will post a random Rick and Morty gif with caption.
 
-`simpsons [quote to search for]`
-Searches for a Simpsons quote using all text following the command, grabs the first result and sends the resulting quote with accompanying gif.
-
-`simpsonsgif`
-Will post a random Simpsons quote with accompanying gif.
-
-`futurama`
-Will post a random Futurama quote with accompanying picture.
-
-`futurama [quote to search for]`
-Searches for a Futurama quote using all text following the command, grabs the first result and sends the resulting quote with accompanying gif.
-
-`futuramagif`
-Will post a random Futurama quote with accompanying gif.
-
-`rickandmorty`
-Will post a random Rick and Morty quote with accompanying picture.
-
-`rickandmorty [quote to search for]`
-Searches for a Rick and Morty quote using all text following the command, grabs the first result and sends the resulting quote with accompanying gif.
-
-`rickandmortygif`
-Will post a random Rick and Morty quote with accompanying gif.
-
-Tip: You can also use diddly and doodly as a command prefix when you really want to flaunt those Flanders-isms.
-(Usage: `diddly help`, `doodly help`, `diddly-help`, `doodly-help`)
+**For Example:** `ned info`, `diddly help`, `doodly-simpsons`
 '''
 
-botInfo = ('''Hi-diddly-ho, neighborino! I hear you wanted some mor-diddly-ore
-information... Well, If it's clear and yella', you've got juice there, fella.
-If it's tangy and brown, you're in cider town.
+botInfo = ('''Hi-diddly-ho, neighborino! I hear you wanted some mor-diddly-ore information...
+Well, If it's clear and yella', you've got juice there, fella. If it's tangy and brown, you're in cider town.
 Now, there's two exceptions and it gets kinda tricky here...
-
-Tip: Use ned help or <@221609683562135553> help for a full list of commands.
 
 __**INFO**__
 
