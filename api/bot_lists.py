4d1acee801015b9deea75b5603eb99bea5a95b55
Mitch Woollatt <21175347+MitchellAW@users.noreply.github.com>
2018-02-10 14:54:08 +1030

    Fixed bug with bots.discord.pw

--- api/bot_lists.py
+++ api/bot_lists.py
@@ -11,14 +11,15 @@ async def update_guild_count(bot, api_url, token):
     payload = {"server_count": len(bot.guilds)}
 
     async with aiohttp.ClientSession() as aio_client:
-        resp = await aio_client.post(api_url, data=payload, headers=headers)
+        resp = await aio_client.post(api_url, data=payload,
+                                     json=payload, headers=headers)
         print(await resp.text())
 
 
 # Get a list of user IDs who have upvoted FlandersBOT
 async def get_upvoters():
     async with aiohttp.ClientSession() as aio_client:
-        db_headers = {"Authorization" : settings.config.DB_TOKEN}
+        db_headers = {"Authorization": settings.config.DB_TOKEN}
         resp = await aio_client.get('https://discordbots.org/api/bots/2216096' +
                                     '83562135553/votes?onlyids=true',
                                     headers=db_headers)
