09e6d8c6019be48806b15934e986c17fd036dcad
Mitch Woollatt <21175347+MitchellAW@users.noreply.github.com>
2018-02-22 17:10:35 +1030

    Fixed typos in help command output

--- botInfo.py
+++ botInfo.py
@@ -27,7 +27,7 @@ I'll happily oblige! Well, so long as the reverend approves of course.
 **TV SHOWS**
 
 **help tvshows** - Will post a list of commands for all currently supported''' +
-                '''TV Shows
+                ''' TV shows.
 
 *I currently support commands for The Simpsons, Futurama, Rick and Morty, 
 30 Rock and West Wing.*
