40782775133fe2448fc87296518bbb7bf0f02201
Mitch Woollatt <21175347+MitchellAW@users.noreply.github.com>
2018-03-09 18:20:46 +1030

    Added arg to post_gif for specifying whether to generate the gif

--- cogs/tvshows.py
+++ cogs/tvshows.py
@@ -7,12 +7,12 @@ class TVShowCog:
         self.api = api
 
     # Post a random screencap
-    async def post_image(self, ctx, search_text=None, caption=None):
-        if search_text is None:
+    async def post_image(self, ctx, search=None, caption=None):
+        if search is None:
             screencap = await self.api.get_random_screencap()
 
         else:
-            screencap = await self.api.search_for_screencap(search_text)
+            screencap = await self.api.search_for_screencap(search)
 
         if screencap is not None:
             ctx.bot.cached_screencaps.update(
@@ -20,28 +20,33 @@ class TVShowCog:
             )
             await ctx.send(screencap.get_meme_url(caption))
 
-    # Post generating message, generate gif then post generated Url
-    async def post_gif(self, ctx, search_text=None, caption=None):
-        if search_text is None:
+    # Post a gif, if generating, post generating loading message and then edit
+    # message to include gif with the generated url
+    async def post_gif(self, ctx, search=None, caption=None, generate=True):
+        if search is None:
             screencap = await self.api.get_random_screencap()
 
         else:
-            screencap = await self.api.search_for_screencap(search_text)
+            screencap = await self.api.search_for_screencap(search)
 
         if screencap is not None:
             ctx.bot.cached_screencaps.update(
                 {ctx.message.channel.id: screencap}
             )
             gif_url = await screencap.get_gif_url(caption)
-            sent = await ctx.send('Generating {}... '.format(screencap.key)
-                                  + '<a:loading:410316176510418955>')
 
-            generated_url = await self.api.generate_gif(gif_url)
-            await sent.edit(content=generated_url)
+            if generate:
+                sent = await ctx.send('Generating {}... '.format(screencap.key)
+                                      + '<a:loading:410316176510418955>')
+                generated_url = await self.api.generate_gif(gif_url)
+                await sent.edit(content=generated_url)
+
+            else:
+                await ctx.send(gif_url)
 
     # Ask user to post a custom caption, take custom caption and generate
     # custom gif, then delete request for caption and post custom gif
-    async def post_custom_gif(self, ctx, search_text=None):
+    async def post_custom_gif(self, ctx, search=None):
         first = await ctx.send('Please post the caption you would like to use',
                                delete_after=30)
         try:
@@ -50,7 +55,7 @@ class TVShowCog:
 
             resp = await self.bot.wait_for('message', check=check, timeout=30)
             await first.delete()
-            await self.post_gif(ctx, search_text, resp.content)
+            await self.post_gif(ctx, search, resp.content)
 
         except asyncio.TimeoutError:
             pass
