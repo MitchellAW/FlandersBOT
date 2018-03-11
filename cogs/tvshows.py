e274ada5eed0d952a528382ce52d0db211889bc1
Mitch Woollatt <21175347+MitchellAW@users.noreply.github.com>
2018-03-12 08:01:00 +1030

    Refactored cogs/tvshows.py, now catches all APIPageStatusErrors

--- cogs/tvshows.py
+++ cogs/tvshows.py
@@ -1,5 +1,3 @@
-import asyncio
-
 import compuglobal
 
 
@@ -8,51 +6,43 @@ class TVShowCog:
         self.bot = bot
         self.api = api
 
-    # Post a random screencap
-    async def post_image(self, ctx, search=None, caption=None):
-        if search is None:
-            screencap = await self.api.get_random_screencap()
+    # Get random or searched screencap based on search parameter and update
+    # cached_screencaps
+    async def get_screencap(self, ctx, search=None):
+        screencap = None
+        try:
+            if search is None:
+                screencap = await self.api.get_random_screencap()
 
-        else:
-            try:
+            else:
                 screencap = await self.api.search_for_screencap(search)
 
-            except compuglobal.APIPageStatusError as error:
-                await ctx.send(error)
-                return
-
-            except compuglobal.NoSearchResultsFound as error:
-                await ctx.send(error)
-                return
-
-        if screencap is not None:
-            ctx.bot.cached_screencaps.update(
+            self.bot.cached_screencaps.update(
                 {ctx.message.channel.id: screencap}
             )
-            await ctx.send(screencap.get_meme_url(caption))
+
+        except compuglobal.APIPageStatusError as error:
+            await self.bot.LOGGING.send(error)
+            await ctx.send(error)
+
+        except compuglobal.NoSearchResultsFound as error:
+            await ctx.send(error)
+
+        return screencap
+
+    # Post a random screencap image with caption
+    async def post_image(self, ctx, search=None, caption=None):
+        screencap = await self.get_screencap(ctx, search)
+
+        if screencap is not None:
+            await ctx.send(await screencap.get_meme_url(caption))
 
     # Post a gif, if generating, post generating loading message and then edit
     # message to include gif with the generated url
     async def post_gif(self, ctx, search=None, caption=None, generate=True):
-        if search is None:
-            screencap = await self.api.get_random_screencap()
-
-        else:
-            try:
-                screencap = await self.api.search_for_screencap(search)
-
-            except compuglobal.APIPageStatusError as error:
-                await ctx.send(error)
-                return
-
-            except compuglobal.NoSearchResultsFound as error:
-                await ctx.send(error)
-                return
+        screencap = await self.get_screencap(ctx, search)
 
         if screencap is not None:
-            ctx.bot.cached_screencaps.update(
-                {ctx.message.channel.id: screencap}
-            )
             gif_url = await screencap.get_gif_url(caption)
 
             if generate:
@@ -63,19 +53,3 @@ class TVShowCog:
 
             else:
                 await ctx.send(gif_url)
-
-    # Ask user to post a custom caption, take custom caption and generate
-    # custom gif, then delete request for caption and post custom gif
-    async def post_custom_gif(self, ctx, search=None):
-        first = await ctx.send('Please post the caption you would like to use',
-                               delete_after=30)
-        try:
-            def check(message):
-                return message.author == ctx.message.author
-
-            resp = await self.bot.wait_for('message', check=check, timeout=30)
-            await first.delete()
-            await self.post_gif(ctx, search, resp.content)
-
-        except asyncio.TimeoutError:
-            pass
