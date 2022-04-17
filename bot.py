1ac767f11a97d9fca6f09a6db160c3d31560cdf9
Mitch Woollatt <MitchellAW@users.noreply.github.com>
2022-04-17 14:49:21 +0930

    Moved setup tasks to new setup_hook method

--- bot.py
+++ bot.py
@@ -19,6 +19,7 @@ class FlandersBOT(commands.Bot):
         self.cached_screencaps = {}
         self.reminders = []
         self.uptime = datetime.utcnow()
+        self.db = None
 
         # Load config file
         with open('settings/config.json', 'r') as config_file:
@@ -26,6 +27,28 @@ class FlandersBOT(commands.Bot):
 
         # Configure debug mode
         self.debug_mode = self.config['debug_mode']
+
+    async def setup_hook(self):
+        # Establish connection to PostgreSQL database
+        try:
+            self.db = await asyncpg.create_pool(**self.config['db_credentials'])
+
+        except Exception as e:
+            exc = f'{type(e).__name__}: {e}'
+            print(f'Failed to connect PostgreSQL. Terminating.\n{exc}')
+            exit()
+
+        # Load all bot extensions from cogs folder
+        for file in os.listdir("cogs"):
+            if file.endswith(".py"):
+                extension = file[:-3]
+
+                try:
+                    await self.load_extension(f"cogs.{extension}")
+
+                except Exception as e:
+                    exc = f'{type(e).__name__}: {e}'
+                    print(f'Failed to load extension {extension}\n{exc}')
 
     # Default get prefixes method, is replaced once prefixes cog loads (custom guild prefixes)
     async def get_default_prefixes(self, bot, message):
@@ -35,39 +58,15 @@ class FlandersBOT(commands.Bot):
 
 # Runs FlandersBOT
 def run_bot():
-    loop = asyncio.get_event_loop()
-
-    # Load config file for token
-    with open('settings/config.json', 'r') as conf:
-        config = json.load(conf)
-
-    # Establish connection to PostgreSQL database
-    try:
-        db = loop.run_until_complete(asyncpg.create_pool(**config['db_credentials']))
-
-    except Exception as e:
-        exc = f'{type(e).__name__}: {e}'
-        print(f'Failed to connect PostgreSQL. Terminating.\n{exc}')
-        return
-
     # Requires members intents for leaderboard username display
     intents = discord.Intents.default()
 
     # Initialise bot with db connection
     bot = FlandersBOT(intents)
-    bot.db = db
 
-    # Load all bot extensions from cogs folder
-    for file in os.listdir("cogs"):
-        if file.endswith(".py"):
-            extension = file[:-3]
-
-            try:
-                bot.load_extension(f"cogs.{extension}")
-
-            except Exception as e:
-                exc = f'{type(e).__name__}: {e}'
-                print(f'Failed to load extension {extension}\n{exc}')
+    # Load config file for token
+    with open('settings/config.json', 'r') as conf:
+        config = json.load(conf)
 
     # Run FlandersBOT
     bot.run(config['bot_token'])
