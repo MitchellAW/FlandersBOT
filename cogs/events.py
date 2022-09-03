fe0e4015727fccd1ca08660b91f243e4a7ca65a9
Mitch Woollatt <MitchellAW@users.noreply.github.com>
2022-09-03 22:17:22 +0930

    Added slash commands to bot status cycle

--- cogs/events.py
+++ cogs/events.py
@@ -14,7 +14,7 @@ class Events(commands.Cog):
 
         # Default status configuration
         self.status_index = 0
-        self.status_formats = [f'@{self.bot.user.name} help']
+        self.status_formats = ['/simpsons | {} servers', '/futurama | {} servers', '/rickandmorty | {} servers']
 
         # Discord channel ids used for all error logging
         self.LOGGING_CHANNEL = 797662079573557250
