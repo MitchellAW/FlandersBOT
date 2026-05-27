abcd176606e467e566ca4161a856868f681c99c7
Mitch Woollatt <21175347+MitchellAW@users.noreply.github.com>
2026-05-27 15:29:52 +0930

    Add pydantic settings config for env file

---
+++ settings/config.py
@@ -0,0 +1,25 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class FlandersConfig(BaseSettings, frozen=True):
+    # Use .env file
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    # Bot configuration
+    bot_token: str
+    owner_id: int
+    debug_mode: bool = True
+
+    # Database configuration
+    postgres_host: str
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+    postgres_port: int
+
+    @property
+    def postgres_dsn(self) -> str:
+        return (
+            f"postgres://{self.postgres_user}:{self.postgres_password}"
+            f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+        )
